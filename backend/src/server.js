1c1ac03fd1342a26081811682be00ba404d55448
dparmar <116805076+dparmar04@users.noreply.github.com>
2025-06-18 12:18:58 +0530

    fixed production

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,11 +23,14 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-if (process.env.NODE_ENV = "production") { }
-app.use(express.static(path.join(__dirname, "../frontend/dist")))
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-})
+if (process.env.NODE_ENV = "production") {
+
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+  })
+}
 
 
 connectDB().then(() => {
